ad392a849e4569331f8b1e62a4e3678e13dc6704
Dilan Coss <dilanarma@hotmail.com>
2018-08-02 20:50:50 -0600

    Adds logger and changes rabbitmq port

--- src/main/java/com/skytask/listener/Listener.java
+++ src/main/java/com/skytask/listener/Listener.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.type.CollectionType;
 import com.skytask.channel.ProductSource;
 import com.skytask.model.Product;
 import com.skytask.service.IProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -15,12 +17,14 @@ import java.util.List;
 @EnableBinding(ProductSource.class)
 public class Listener {
 
+    private static Logger logger = LoggerFactory.getLogger(Listener.class);
+
     @Autowired
     private IProductService productService;
 
     @StreamListener("productListChannelInput")
-    public void gettingProduct(String products) throws IOException {
-        System.out.println("I received: " + products);
+    public void updateProductList(String products) throws IOException {
+        logger.info("I received: " + products);
         ObjectMapper mapper = new ObjectMapper();
         CollectionType type = mapper.getTypeFactory().constructCollectionType(List.class, Product.class);
         productService.setProducts(mapper.readValue(products, type));
